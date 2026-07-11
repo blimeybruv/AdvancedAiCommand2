c56791cb67fb2b6712e22d6a12e393c135bec3c5
SteamNimmersatt <nimmersatt@zebramail.eu>
2026-07-11 11:39:42 +0200

    Add build script, prepare release 2.6.0.0

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+HEMTT_PATH="./hemtt"
+
+# Check if parameter is missing
+if [ -z "$1" ]; then
+    echo "Error: Missing parameter. Please specify either 'dev' or 'release'."
+    echo "Usage: $0 <dev|release>"
+    exit 1
+fi
+
+BUILD_TYPE="$1"
+
+# Validate that the parameter is strictly 'dev' or 'release'
+if [[ "$BUILD_TYPE" != "dev" && "$BUILD_TYPE" != "release" ]]; then
+    echo "Error: Invalid parameter '$BUILD_TYPE'."
+    echo "Usage: $0 <dev|release>"
+    exit 1
+fi
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S.%3N') $1"
+}
+
+download_hemtt() {
+    if [ ! -f "$HEMTT_PATH" ]; then
+        log "Downloading hemtt..."
+        wget -O "$HEMTT_PATH" https://github.com/BrettMayson/HEMTT/releases/latest/download/linux-x64
+        chmod +x "$HEMTT_PATH"
+    fi
+}
+
+build_mod() {
+    log "------------------Building the mod ($BUILD_TYPE).------------------"
+    "$HEMTT_PATH" "$BUILD_TYPE" # For 'dev', this will create a symbolic link at "<arma3 dir>/z/aicommand2" which allows us to load the mod from this directory.
+}
+
+download_hemtt
+build_mod
